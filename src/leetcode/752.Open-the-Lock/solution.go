169bccc436eb2fc7d1e46aaf464801d788a152cf
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-04-22 23:49:38 +0400

    752. Open the Lock (fast solution)

--- src/leetcode/752.Open-the-Lock/solution.go
+++ src/leetcode/752.Open-the-Lock/solution.go
@@ -3,8 +3,8 @@ package _52_Open_the_Lock
 import "strconv"
 
 func openLock(deadends []string, target string) int {
-	t, _ := strconv.Atoi(target)
-	used := make([]bool, 10000)
+	const N = 10000
+	used := make([]bool, N)
 	for i := 0; i < len(deadends); i++ {
 		item, _ := strconv.Atoi(deadends[i])
 		if item == 0 {
@@ -12,32 +12,34 @@ func openLock(deadends []string, target string) int {
 		}
 		used[item] = true
 	}
-	q := make([]int, 0)
-	q = append(q, 0)
+	queueArr := [N]int{}
+	queue := queueArr[:1]
 	used[0] = true
+
+	tryEnqueue := func(q []int, i int) []int {
+		if !used[i] {
+			q = append(q, i)
+			used[i] = true
+		}
+		return q
+	}
+
+	targetNum, _ := strconv.Atoi(target)
 	length := 0
-	for len(q) > 0 {
-		items := make([]int, len(q))
-		copy(items, q)
-		q = nil
+	for len(queue) > 0 {
+		items := make([]int, len(queue))
+		copy(items, queue)
+		queue = queueArr[:0]
 		for i := 0; i < len(items); i++ {
-			item := items[i]
-			if item == t {
+			num := items[i]
+			if num == targetNum {
 				return length
 			}
 			k := 1
 			for i := 0; i < 4; i++ {
-				d := item / k % 10
-				next1 := item - d*k + (d+1)%10*k
-				next2 := item - d*k + (10+(d-1))%10*k
-				if !used[next1] {
-					q = append(q, next1)
-					used[next1] = true
-				}
-				if !used[next2] {
-					q = append(q, next2)
-					used[next2] = true
-				}
+				d := num / k % 10
+				queue = tryEnqueue(queue, num+((d+1)%10-d)*k)
+				queue = tryEnqueue(queue, num+((d+9)%10-d)*k)
 				k *= 10
 			}
 		}
