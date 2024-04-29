104ba1da7773a74a1b84056e3dfb227b069aed0e
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-04-29 16:32:11 +0400

    2997. Minimum Number of Operations to Make Array XOR Equal to K (short solution)

--- src/leetcode/n2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/solution.go
+++ src/leetcode/n2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/solution.go
@@ -1,15 +1,11 @@
 package n2997_minimum_number_of_operations_to_make_array_xor_equal_to_k
 
+import "math/bits"
+
 func minOperations(nums []int, k int) int {
 	xor := 0
-	for i := 0; i < len(nums); i++ {
-		xor ^= nums[i]
+	for _, num := range nums {
+		xor ^= num
 	}
-	ans := 0
-	diff := xor ^ k
-	for diff != 0 {
-		ans += diff & 1
-		diff >>= 1
-	}
-	return ans
+	return bits.OnesCount(uint(xor ^ k))
 }
