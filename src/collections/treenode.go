1a37318c92ba989cce666f722078ff97ef139251
Mike Gorodilov <mihailgorodilov@gmail.com>
2024-05-17 10:49:09 +0400

    treenode refactored

--- src/collections/treenode.go
+++ src/collections/treenode.go
@@ -12,13 +12,13 @@ type TreeNode struct {
 }
 
 func TreeToStr(root *TreeNode) string {
-	arr := TreeToNodeArr(root)
+	arr := treeToNodeArr(root)
 	for len(arr) > 0 && arr[len(arr)-1] == nil {
 		arr = arr[:len(arr)-1]
 	}
 	result := make([]string, 0, len(arr))
 	for _, node := range arr {
-		result = append(result, ValToStr(node))
+		result = append(result, valToStr(node))
 	}
 	return strings.Join(result, ",")
 }
@@ -27,10 +27,10 @@ func StrToTree(s string) *TreeNode {
 	if s == "" {
 		return nil
 	}
-	return StrArrToTree(strings.Split(s, ","), 0)
+	return strArrToTree(strings.Split(s, ","), 0)
 }
 
-func TreeToNodeArr(root *TreeNode) []*TreeNode {
+func treeToNodeArr(root *TreeNode) []*TreeNode {
 	result := make([]*TreeNode, 0)
 	result = append(result, root)
 	offset := 0
@@ -46,20 +46,20 @@ func TreeToNodeArr(root *TreeNode) []*TreeNode {
 	return result
 }
 
-func ValToStr(node *TreeNode) string {
+func valToStr(node *TreeNode) string {
 	if node == nil {
 		return "null"
 	}
 	return strconv.Itoa(node.Val)
 }
 
-func StrArrToTree(values []string, i int) *TreeNode {
+func strArrToTree(values []string, i int) *TreeNode {
 	if i >= len(values) || values[i] == "null" {
 		return nil
 	}
 	val, _ := strconv.Atoi(values[i])
 	node := &TreeNode{Val: val}
-	node.Left = StrArrToTree(values, 2*i+1)
-	node.Right = StrArrToTree(values, 2*i+2)
+	node.Left = strArrToTree(values, 2*i+1)
+	node.Right = strArrToTree(values, 2*i+2)
 	return node
 }
