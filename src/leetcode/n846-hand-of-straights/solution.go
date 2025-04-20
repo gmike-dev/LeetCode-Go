37ca94498f28a9bc4ebd0f3696fc3822ead30d56
Mike Gorodilov <mihailgorodilov@gmail.com>
2025-04-20 16:11:04 +0400

    846. Hand of Straights

--- src/leetcode/n846-hand-of-straights/solution.go
+++ src/leetcode/n846-hand-of-straights/solution.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "math"
+
 func isNStraightHand(hand []int, groupSize int) bool {
 	if groupSize == 1 {
 		return true
@@ -9,4 +11,27 @@ func isNStraightHand(hand []int, groupSize int) bool {
 		return false
 	}
 
+	m := make(map[int]int)
+	for _, val := range hand {
+		m[val]++
+	}
+
+	for i := 0; i < n/groupSize; i++ {
+		x := math.MaxInt
+		for key := range m {
+			x = min(x, key)
+		}
+		x--
+		for j := 0; j < groupSize; j++ {
+			x++
+			if m[x] == 0 {
+				return false
+			}
+			m[x]--
+			if m[x] == 0 {
+				delete(m, x)
+			}
+		}
+	}
+	return true
 }
