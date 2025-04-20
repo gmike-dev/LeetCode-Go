854ea4ecd2337b3df1b26a2bb49ae1e56dbf73a4
Mike Gorodilov <mihailgorodilov@gmail.com>
2025-04-20 16:03:49 +0400

    heap

--- src/collections/heap.go
+++ src/collections/heap.go
@@ -2,24 +2,48 @@ package collections
 
 import "cmp"
 
-func BuildMaxHeap(a []int) {
+func compareMax[T cmp.Ordered](x, y T) int {
+	return cmp.Compare(x, y)
+}
+
+func compareMin[T cmp.Ordered](x, y T) int {
+	return -cmp.Compare(x, y)
+}
+
+func BuildMaxHeap[T cmp.Ordered](a []T) {
 	n := len(a)
 	for i := n/2 - 1; i >= 0; i-- {
-		PushDown(a, i, n, cmp.Compare[int])
+		PushDown(a, i, compareMax[T])
 	}
 }
 
-func BuildMinHeap(a []int) {
+func BuildMinHeap[T cmp.Ordered](a []T) {
 	n := len(a)
-	invCompare := func(x, y int) int {
-		return -cmp.Compare(x, y)
-	}
 	for i := n/2 - 1; i >= 0; i-- {
-		PushDown(a, i, n, invCompare)
+		PushDown(a, i, compareMin[T])
 	}
 }
 
-func PushDown(a []int, i, n int, cmp func(int, int) int) {
+func PopMax[T cmp.Ordered](a []T) (T, []T) {
+	n := len(a)
+	result := a[0]
+	a[0], a[n-1] = a[n-1], a[0]
+	a = a[:len(a)-1]
+	PushDown(a, 0, compareMax[T])
+	return result, a
+}
+
+func PopMin[T cmp.Ordered](a []T) (T, []T) {
+	n := len(a)
+	result := a[0]
+	a[0], a[n-1] = a[n-1], a[0]
+	a = a[:len(a)-1]
+	PushDown(a, 0, compareMin[T])
+	return result, a
+}
+
+func PushDown[T cmp.Ordered](a []T, i int, cmp func(T, T) int) {
+	n := len(a)
 	for {
 		largest := i
 		l := 2*i + 1
--- src/collections/heap_test.go
+++ src/collections/heap_test.go
@@ -2,6 +2,7 @@ package collections
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 )
 
@@ -15,4 +16,58 @@ func TestBuildMaxHeap2(t *testing.T) {
 	heap := []int{4, 1, 2, 3, 65, 7, 2, 3, 1, 2, 3, 6, -1, 23, 0, 0, -13}
 	BuildMaxHeap(heap)
 	assert.Equal(t, []int{65, 4, 23, 3, 3, 7, 2, 3, 1, 2, 1, 6, -1, 2, 0, 0, -13}, heap)
+}
+
+func TestBuildMinHeap1(t *testing.T) {
+	heap := []int{1, 2, 3}
+	BuildMinHeap(heap)
+	assert.Equal(t, []int{1, 2, 3}, heap)
+}
+
+func TestBuildMinHeap2(t *testing.T) {
+	heap := []int{4, 1, 2, 3, 65, 7, 2, 3, 1, 2, 3, 6, -1, 23, 0, 0, -13}
+	BuildMinHeap(heap)
+	assert.Equal(t, []int{-13, 0, -1, 1, 2, 2, 0, 3, 1, 65, 3, 6, 7, 23, 2, 4, 3}, heap)
+}
+
+func TestPopMax(t *testing.T) {
+	heap := []int{4, 1, 2, 3, 65, 7, 2, 3, 1, 2, 3, 6, -1, 23, 0, 0, -13}
+	BuildMaxHeap(heap)
+
+	n := len(heap)
+	for i := 0; i < n; i++ {
+		var actual, expected int
+		expected = getMax(heap)
+		actual, heap = PopMax(heap)
+		assert.Equal(t, expected, actual)
+	}
+}
+
+func TestPopMin(t *testing.T) {
+	heap := []int{4, 1, 2, 3, 65, 7, 2, 3, 1, 2, 3, 6, -1, 23, 0, 0, -13}
+	BuildMinHeap(heap)
+
+	n := len(heap)
+	for i := 0; i < n; i++ {
+		var actual, expected int
+		expected = getMin(heap)
+		actual, heap = PopMin(heap)
+		assert.Equal(t, expected, actual)
+	}
+}
+
+func getMax(a []int) int {
+	result := math.MinInt
+	for _, val := range a {
+		result = max(result, val)
+	}
+	return result
+}
+
+func getMin(a []int) int {
+	result := math.MaxInt
+	for _, val := range a {
+		result = min(result, val)
+	}
+	return result
 }
