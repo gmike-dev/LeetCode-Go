5c0653874a664a36a84c6186ccbb126d569b6dd1
Mike Gorodilov <mihailgorodilov@gmail.com>
2025-05-20 11:51:58 +0400

    1931. Painting a Grid With Three Different Colors (fast solution)

--- src/leetcode/_dp/n1931-painting-a-grid-with-three-different-colors/solution.go
+++ src/leetcode/_dp/n1931-painting-a-grid-with-three-different-colors/solution.go
@@ -1,46 +1,24 @@
 package n1931_painting_a_grid_with_three_different_colors
 
-import "math"
-
 func colorTheGrid(m int, n int) int {
 	const Mod = int(1e9) + 7
-	mc := int(math.Pow(3, float64(m)))
-	isDistinct := make([]bool, mc)
-	for i := 0; i < mc; i++ {
-		isDistinct[i] = true
-		x := i
-		for j := 0; j < m-1; j++ {
-			if x%3 == x/3%3 {
-				isDistinct[i] = false
-				break
-			}
-			x /= 3
+	var pow3 = [...]int{1, 3, 9, 27, 81, 243}
+	var distinct []int
+	for i := 0; i < pow3[m]; i++ {
+		if isDistinct(i, m) {
+			distinct = append(distinct, i)
 		}
 	}
-
+	mc := len(distinct)
 	canJoin := make([][]bool, mc)
 	for i := 0; i < mc; i++ {
 		canJoin[i] = make([]bool, mc)
 	}
 	for i := 0; i < mc; i++ {
-		if !isDistinct[i] {
-			continue
-		}
 		for j := 0; j < i; j++ {
-			if !isDistinct[j] {
-				continue
-			}
-			canJoin[i][j] = true
-			canJoin[j][i] = true
-			x, y := i, j
-			for k := 0; k < m; k++ {
-				if x%3 == y%3 {
-					canJoin[i][j] = false
-					canJoin[j][i] = false
-					break
-				}
-				x /= 3
-				y /= 3
+			if isCompatible(distinct[i], distinct[j], m) {
+				canJoin[i][j] = true
+				canJoin[j][i] = true
 			}
 		}
 	}
@@ -49,15 +27,10 @@ func colorTheGrid(m int, n int) int {
 		dp[i] = make([]int, mc)
 	}
 	for i := 0; i < mc; i++ {
-		if isDistinct[i] {
-			dp[0][i] = 1
-		}
+		dp[0][i] = 1
 	}
 	for i := 1; i < n; i++ {
 		for j := 0; j < mc; j++ {
-			if !isDistinct[j] {
-				continue
-			}
 			for k := 0; k < j; k++ {
 				if canJoin[j][k] {
 					dp[i][j] = (dp[i][j] + dp[i-1][k]) % Mod
@@ -71,4 +44,25 @@ func colorTheGrid(m int, n int) int {
 		sum = (sum + dp[n-1][i]) % Mod
 	}
 	return sum
+}
+
+func isDistinct(x, m int) bool {
+	for j := 0; j < m-1; j++ {
+		if x%3 == x/3%3 {
+			return false
+		}
+		x /= 3
+	}
+	return true
+}
+
+func isCompatible(x, y, m int) bool {
+	for k := 0; k < m; k++ {
+		if x%3 == y%3 {
+			return false
+		}
+		x /= 3
+		y /= 3
+	}
+	return true
 }
