ff93297a8f9629bf297e998adfc7ddeed906795f
Mike Gorodilov <mihailgorodilov@gmail.com>
2025-07-20 01:21:19 +0400

    1233. Remove Sub-Folders from the Filesystem (trie refactoring)

--- src/leetcode/_strings/n1233-remove-sub-folders-from-the-filesystem/trie/solution.go
+++ src/leetcode/_strings/n1233-remove-sub-folders-from-the-filesystem/trie/solution.go
@@ -4,40 +4,38 @@ import "strings"
 
 func removeSubfolders(folder []string) []string {
 	root := newTrieNode("")
-	for _, f := range folder {
+	add := func(f string) {
 		node := root
 		s := strings.Split(f, "/")
-		substring := false
-		for _, sub := range s {
-			if sub != "" {
-				child, ok := node.child[sub]
-				if ok {
-					if child.folder != "" {
-						substring = true
-						break
-					}
-				} else {
-					child = newTrieNode(sub)
-					node.child[sub] = child
-				}
-				node = child
+		for i := 1; i < len(s); i++ {
+			sub := s[i]
+			child, ok := node.child[sub]
+			if !ok {
+				child = newTrieNode(sub)
+				node.child[sub] = child
+			} else if child.folder != "" {
+				return
 			}
-		}
-		if substring {
-			continue
+			node = child
 		}
 		node.folder = f
+		node.child = nil
 	}
-	return collectFolders(root, []string{})
-}
-
-func collectFolders(node *trieNode, res []string) []string {
-	if node.folder != "" {
-		return append(res, node.folder)
+	for _, f := range folder {
+		add(f)
 	}
-	for _, v := range node.child {
-		res = collectFolders(v, res)
+	var res []string
+	var collect func(*trieNode)
+	collect = func(node *trieNode) {
+		if node.folder != "" {
+			res = append(res, node.folder)
+		} else {
+			for _, v := range node.child {
+				collect(v)
+			}
+		}
 	}
+	collect(root)
 	return res
 }
 
