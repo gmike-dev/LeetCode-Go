683c8b78520ffa7e793a9120d21e246c1cb7e8a9
Mike Gorodilov <mihailgorodilov@gmail.com>
2026-01-11 13:18:03 +0400

    85. Maximal Rectangle (short monotonic stack solution)

--- src/leetcode/_dp/n85-maximal-rectangle/monotonic-stack/solution.go
+++ src/leetcode/_dp/n85-maximal-rectangle/monotonic-stack/solution.go
@@ -3,28 +3,26 @@ package monotonic_stack
 func maximalRectangle(a [][]byte) int {
 	n, m := len(a), len(a[0])
 	h := make([]int, m+1)
-	res := 0
-	sh := make([]int, m+1)
-	si := make([]int, m+1)
+	s := make([]int, m+1)
 	sn := 1
+	res := 0
 	for i := 1; i <= n; i++ {
+		s[0] = 0
 		sn = 1
 		for j := 1; j <= m; j++ {
 			if a[i-1][j-1] != '0' {
 				h[j]++
-				k := j
-				for sh[sn-1] >= h[j] {
-					k = si[sn-1]
+				for h[s[sn-1]] >= h[j] {
 					sn--
 				}
-				sh[sn] = h[j]
-				si[sn] = k
+				s[sn] = j
 				sn++
-				for l := range sn {
-					res = max(res, (j-si[l]+1)*sh[l])
+				for k := 1; k < sn; k++ {
+					res = max(res, (j-s[k-1])*h[s[k]])
 				}
 			} else {
 				h[j] = 0
+				s[0] = j
 				sn = 1
 			}
 		}
